b4e3d821988c6cd3f7d582082638c301609c9543
Jerry2310 <as1286052@gamil.com>
2023-12-22 23:19:44 +0530

    first commit

---
+++ client/src/context/index.jsx
@@ -0,0 +1,113 @@
+import React, { useState, useContext, createContext } from "react";
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+
+const StateContext = createContext();
+
+
+export const StateContextProvider = ({ children }) => {
+  const { contract, isLoading } = useContract('0x74BF79E312f0BDce88De237C61FD517A47C25E78', { chainId: 137 });
+  // 0x74BF79E312f0BDce88De237C61FD517A47C25E78
+    const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+
+    const address = useAddress();
+    const connect = useMetamask();
+
+    const publishCampaign = async (form) => {
+        try {
+          const data = await createCampaign({
+                    args: [
+                        address, // owner
+                        form.title, // title
+                        form.description, // description
+                        form.target,
+                        new Date(form.deadline).getTime(), // deadline,
+                        form.image,
+                    ],
+                });
+    
+          console.log("contract call success", data)
+        } catch (error) {
+          console.log("contract call failure", error)
+        }
+      }
+
+
+      const getCampaigns = async () => {
+        const campaigns = await contract.call('getCampaigns');
+    
+        const parsedCampaings = campaigns.map((campaign, i) => ({
+          owner: campaign.owner,
+          title: campaign.title,
+          description: campaign.description,
+          target: ethers.utils.formatEther(campaign.target.toString()),
+          deadline: campaign.deadline.toNumber(),
+          amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+          image: campaign.image,
+          pId: i
+        }));
+    
+        return parsedCampaings;
+      }
+
+      const getUserCampaigns = async () => {
+        const allCampaigns = await getCampaigns();
+    
+        const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+    
+        return filteredCampaigns;
+      }
+
+      const donate = async (pId, amount) => {
+        const data = await contract.call('donateToCampaign', [pId], { value: ethers.utils.parseEther(amount)});
+    
+        return data;
+      }
+
+      const getDonations = async (pId) => {
+        const donations = await contract.call('getDonators', [pId]);
+        const numberOfDonations = donations[0].length;
+    
+        const parsedDonations = [];
+    
+        for(let i = 0; i < numberOfDonations; i++) {
+          parsedDonations.push({
+            donator: donations[0][i],
+            donation: ethers.utils.formatEther(donations[1][i].toString())
+          })
+        }
+    
+        return parsedDonations;
+      }
+
+       // Function to format ether value
+  const formatEtherValue = (value) => {
+    return ethers.utils.parseEther(value.toString()); // Assuming value is a numeric or string value
+  };
+
+  // Function to parse ether value
+  const parseEtherValue = (value) => {
+    return ethers.utils.formatEther(value); // Returns a string value in ether
+  };
+
+      return (
+        <StateContext.Provider
+          value={{ 
+            address,
+            contract,
+            connect,
+            createCampaign: publishCampaign,
+            getCampaigns,
+            getUserCampaigns,
+            donate,
+            getDonations,
+            formatEtherValue,
+            parseEtherValue,
+          }}
+        >
+          {children}
+        </StateContext.Provider>
+      )
+}
+
+export const useStateContext = () => useContext(StateContext);
